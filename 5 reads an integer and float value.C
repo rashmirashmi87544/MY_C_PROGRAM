416c76aa592d7ab9bc5c4cde6bc7604b62ec6340
rashmirashmi87544 <rashmirashmi87544@gmail.com>
2025-11-17 20:48:02 +0530

    Create 5 reads an integer and float value.C

---
+++ 5 reads an integer and float value.C
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    float b;
+    printf("enter an integer value");
+    scanf("%d/n", &a);
+    printf("enter a float value");
+    scanf("%f/n", &b);
+    printf("\nyou entered integer:%d", a);
+    printf("\nyou entered a float value:%f", b);
+    return 0;
+}
