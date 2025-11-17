135fe7971750c622c8bdd151c80db14981b2773a
rashmirashmi87544 <rashmirashmi87544@gmail.com>
2025-11-17 19:46:41 +0530

    Add C program to print name, age, and location

---
+++ print your name age location.C
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    // Write C code here
+    printf("Name: Rashmi L\n");
+    printf("Age: 18 years\n");
+    printf("Location: Nelamangala\n");
+
+    return 0;
+}
