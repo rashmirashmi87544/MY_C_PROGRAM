bff7bd5faf3ea96b46d9fc1b9d5208cba2fc596a
rashmirashmi87544 <rashmirashmi87544@gmail.com>
2025-11-18 11:27:19 +0530

    Create while loop.C

---
+++ while loop.C
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+int n, sum = 0, i = 1;
+printf("Enter a positive integer (N): ");
+scanf("%d", &n);
+while (i <= n) {
+sum += i;
+i++;
+}
+printf("Sum of first %d natural numbers: %d\n", n, sum);
+}
