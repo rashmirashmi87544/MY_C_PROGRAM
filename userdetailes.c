2d3e9fd5f1f2c7ec2aed10a6692436981d7a335f
rashmirashmi87544 <rashmirashmi87544@gmail.com>
2025-11-18 16:14:57 +0530

    Add userdetailes.c to display user information

---
+++ userdetailes.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int userdetailes(){
+    char name,dept;
+    int age;
+    printf("My name is rashmi\n");
+    
+    printf("My age is 18\n");
+    
+    printf(" My dept is CSE\n");
+    
+}
+    int main(){
+        userdetailes();
+    }
