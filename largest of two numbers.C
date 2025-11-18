89811bd3bc3dde3f2b75311b44a14b39ceb1e7bc
rashmirashmi87544 <rashmirashmi87544@gmail.com>
2025-11-18 21:06:12 +0530

    Improve user prompts and output for largest number
    Updated prompts and output formatting for clarity.

--- largest of two numbers.C
+++ largest of two numbers.C
@@ -1,11 +1,14 @@
-#include<stdio.h>
-int main()
-{
+#include <stdio.h>
+
+int main() {
     int a, b;
-    scanf("%d%d", &a,&b);
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+
     if(a > b)
-      printf("largest = %d",a);
+        printf("Largest = %d\n", a);
     else
-      printf("largest =%d",b);
+        printf("Largest = %d\n", b);
+
     return 0;
 }
